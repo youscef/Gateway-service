a302aa836ce5b75e905d5ed804bd70f97f6b00cc
Amraoui Youssef <amraoui.joseph@gmail.com>
2020-12-16 16:28:37 +0100

    Gateway config static

---
+++ src/main/java/org/sid/gatwayservice/GatwayServiceApplication.java
@@ -0,0 +1,13 @@
+package org.sid.gatwayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatwayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatwayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sid/gatwayservice/GatwayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.gatwayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatwayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
